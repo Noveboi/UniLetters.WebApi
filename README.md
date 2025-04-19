61d793abbae586b1d8be2f5e8153ade89267874b
George Nikolaidis <cptgiorgos03@gmail.com>
2025-04-19 14:20:26 +0300

    Initial commit 🥳
    Signed-off-by: George Nikolaidis <cptgiorgos03@gmail.com>

---
+++ README.md
@@ -0,0 +1,37 @@
+# Software Engineering 2025
+
+# Plan (Με βάση το RUP έγγραφο)
+## 1. Τεκμηρίωση & Αρχικά Διαγράμματα (21 - 28 Απριλίου)
+- Όρισμος Προβλήματος (Geo)
+- Αρχικά Διαγράμματα (Kon & Mary)
+- Ανάλυση Απαιτήσεων (Geo)
+
+## 2. UI Design (1η εβδομαδα Μαΐου?)
+- Pages (Mary)
+
+## 3. Αρχική Υλοποίηση (RUP Phase 4 "Εκπόνηση Μελέτης")
+### Τα 9 Διαγράμματα UML (1η εβδομαδα Μαΐου)
+Φάση 4.1 του RUP
+### Setup (1η εβδομαδα Μαΐου?)
+- Setup Docker 
+- Setup EF Core
+- Create Domain Layer
+### Implementation
+- Create UI in Windows Forms
+- Create & Implement use cases
+### Progress Report for Αρχική Υλοποίηση
+Γράφουμε μια σύντομη αναφορά για την αρχική υλοποίηση - τι δουλεύει και τι όχι.
+
+## 4. Τελική Υλοποίηση (RUP Phase 5 "Κατασκευή")
+### Τα 9 Διαγράμματα UML
+Φάση 5.1 του RUP, τελειοποιούμε τα ηδη υπάρχοντα διαγράμμα με μπούσουλα το progress report.
+### Final Implementation
+- Refactor UI (if necessary)
+- Refactor Domain Layer (if necessary)
+- Refactor Use Cases (if necessary)
+### Final Progress Report
+Σύντομη αναφορά για το τελικό προϊόν 😌.
+
+
+# Tech Stack
+- Windows Forms (UI & Backend)
---
+++ UniLetters/UniLetters.Presentation/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace UniLetters.Presentation;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        label1 = new System.Windows.Forms.Label();
+        SuspendLayout();
+        // 
+        // label1
+        // 
+        label1.Location = new System.Drawing.Point(307, 144);
+        label1.Name = "label1";
+        label1.Size = new System.Drawing.Size(177, 27);
+        label1.TabIndex = 0;
+        label1.Text = "Hello There!";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(label1);
+        Text = "Form1";
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.Label label1;
+
+    #endregion
+}
