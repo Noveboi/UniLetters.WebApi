ca8d13c3ac7fe08f5f4bde385dea70f0fd9209da
Nove <cptgiorgos03@gmail.com>
2025-05-29 00:50:39 +0300

    Add student configuration

--- UniLetters/UniLetters.WebApi/Data/Configuration/StudentConfiguration.cs
+++ UniLetters/UniLetters.WebApi/Data/Configuration/StudentConfiguration.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UniLetters.WebApi.Domain;
+
 namespace UniLetters.WebApi.Data.Configuration;
 
-public class StudentConfiguration
+public class StudentConfiguration : IEntityTypeConfiguration<Student>
 {
-    
+    public void Configure(EntityTypeBuilder<Student> builder)
+    {
+        builder.HasKey(x => x.Am);
+
+        builder.Property(x => x.Am).IsUnicode(false).HasMaxLength(10);
+        builder.Property(x => x.Name).HasMaxLength(128);
+    }
 }
