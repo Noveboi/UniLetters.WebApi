1cbefc077fada621775c40ec54d120354c1d91d2
Nove <cptgiorgos03@gmail.com>
2025-05-29 00:32:57 +0300

    Add ToDto() conversion method

---
+++ UniLetters/UniLetters.WebApi/Endpoints/Extensions/ToDtoConversions.cs
@@ -0,0 +1,9 @@
+using UniLetters.WebApi.Domain;
+using UniLetters.WebApi.Endpoints.Dto;
+
+namespace UniLetters.WebApi.Endpoints.Extensions;
+
+public static class ToDtoConversions
+{
+    public static StudentDto ToDto(this Student student) => throw new NotImplementedException();
+}
