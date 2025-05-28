b421130c3de5cf2999f83f2b88588899f46bf7de
Nove <cptgiorgos03@gmail.com>
2025-05-29 01:04:32 +0300

    Add DbSets to DbContext

--- UniLetters/UniLetters.WebApi/Data/UniLettersDbContext.cs
+++ UniLetters/UniLetters.WebApi/Data/UniLettersDbContext.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using UniLetters.WebApi.Domain;
 
 namespace UniLetters.WebApi.Data;
 
 public class UniLettersDbContext(DbContextOptions<UniLettersDbContext> options) : DbContext(options)
 {
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
