fe137f99f5972d9fabaa4563f172a59a6b5e7081
Nove <cptgiorgos03@gmail.com>
2025-05-29 00:50:32 +0300

    Implement student domain model

--- UniLetters/UniLetters.WebApi/Domain/Student.cs
+++ UniLetters/UniLetters.WebApi/Domain/Student.cs
@@ -2,5 +2,14 @@ namespace UniLetters.WebApi.Domain;
 
 public class Student
 {
-    
+    private Student() { }
+    public Student(string am, string name)
+    {
+        Am = am;
+        Name = name;
+    }
+
+    public string Am { get; private init; } = null!;
+    public string Name { get; private init; } = null!;
+    public List<Grade> Grades { get; private init; } = [];
 }
