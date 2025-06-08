90357738f1f6c77332790412b5b554bcd243c138
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-06-08 12:56:38 +0300

    Add ToDto methods for CourseWithGradeDto and StudentCoursesDto

--- UniLetters/UniLetters.WebApi/Endpoints/Extensions/ToDtoConversions.cs
+++ UniLetters/UniLetters.WebApi/Endpoints/Extensions/ToDtoConversions.cs
@@ -12,4 +12,20 @@ public static class ToDtoConversions
             Am: student.Am,
             Average: double.Round(student.Grades.Select(grade => grade.Value).Average(), 2));
     }
+
+    public static CourseWithGradeDto ToDto(this Course course, double grade)
+    {
+        return new CourseWithGradeDto(
+            Id: course.Id,
+            Name: course.Name,
+            Semester: course.Semester,
+            Grade: grade);
+    }
+    
+    public static StudentCoursesDto ToDto(this StudentDto studentDto, List<CourseWithGradeDto> courses)
+    {
+        return new StudentCoursesDto(
+            Student: studentDto,
+            Courses: courses);
+    }
 }
