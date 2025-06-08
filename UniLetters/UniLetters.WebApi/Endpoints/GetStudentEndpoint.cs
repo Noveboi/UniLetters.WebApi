70e38b96cc5227bca7ae0a1494562af6eb999c80
George Nikolaidis <nove@novesoftware.net>
2025-06-08 20:21:42 +0300

    Refactor GetStudent use case

--- UniLetters/UniLetters.WebApi/Endpoints/GetStudentEndpoint.cs
+++ UniLetters/UniLetters.WebApi/Endpoints/GetStudentEndpoint.cs
@@ -20,26 +20,17 @@ public class GetStudentEndpoint(UniLettersDbContext dbContext) : Endpoint<GetStu
     {
         var student = await dbContext.Students
             .Include(s => s.Grades)
-            .Where(s => s.Am == req.Am)
-            .SingleOrDefaultAsync(ct);
+            .ThenInclude(g => g.Course)
+            .FirstOrDefaultAsync(s => s.Am == req.Am, ct);
+
         if (student is null)
-            await SendNotFoundAsync();
-        else
         {
-            List<CourseWithGradeDto> courses = new List<CourseWithGradeDto>();
-            var grades = student.Grades;
-            foreach (var grade in grades)
-            {
-                var course = dbContext.Courses
-                    .Where(c => c.Id == grade.CourseId)
-                    .SingleOrDefaultAsync(ct)
-                    .Result;
-                var courseWithGradeDto = course!.ToDto(grade.Value);
-                courses.Add(courseWithGradeDto);
-            }
-            var studentDto = student.ToDto();
-            var studentCoursesDto = studentDto.ToDto(courses);
-            await SendAsync(studentCoursesDto);
+            await SendNotFoundAsync(ct);
+            return;
         }
+        
+        var studentCoursesDto = student.ToDtoWithCourses();
+        
+        await SendAsync(studentCoursesDto, cancellation: ct);
     }
 }
