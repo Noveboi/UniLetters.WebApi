9e3f0ca2f94c061a83849de2bf3f189f847d79fd
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-06-08 12:49:43 +0300

    Rename StudentId to Am in Grade.cs

--- UniLetters/UniLetters.WebApi/Domain/Grade.cs
+++ UniLetters/UniLetters.WebApi/Domain/Grade.cs
@@ -3,19 +3,19 @@ namespace UniLetters.WebApi.Domain;
 public class Grade
 {
     private Grade() { }
-    public Grade(string studentId, Guid courseId, double value)
+    public Grade(string am, Guid courseId, double value)
     {
         if (value is < 0 or > 10)
             throw new ArgumentOutOfRangeException(nameof(value), "Grade must be between 0 and 10");
         
-        StudentId = studentId;
+        Am = am;
         CourseId = courseId;
         Value = value;
     }
     
     public double Value { get; private init; }
     
-    public string StudentId { get; private init; } = null!;
+    public string Am { get; private init; } = null!;
     public Student Student { get; private init; } = null!;
     
     public Guid CourseId { get; private init; }
