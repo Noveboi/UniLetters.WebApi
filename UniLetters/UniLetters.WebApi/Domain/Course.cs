ed4afb3a953a5015adcfa6cc545f187f2886f507
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-06-08 12:54:30 +0300

    Add semester attribute in Course.cs

--- UniLetters/UniLetters.WebApi/Domain/Course.cs
+++ UniLetters/UniLetters.WebApi/Domain/Course.cs
@@ -3,11 +3,13 @@ namespace UniLetters.WebApi.Domain;
 public class Course
 {
     private Course() => Id = Guid.CreateVersion7();
-    public Course(string name) : this()
+    public Course(string name, int semester) : this()
     {
         Name = name;
+        Semester = semester;
     }
     
     public Guid Id { get; private init; }
     public string Name { get; private init; } = null!;
+    public int Semester { get; private init; }
 }
