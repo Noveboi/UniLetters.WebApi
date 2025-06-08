3d68d3a4c388bf75358851e2cff2b370ad4d009b
George Nikolaidis <nove@novesoftware.net>
2025-06-08 20:19:42 +0300

    Add migration for rename and semester
    Signed-off-by: George Nikolaidis <nove@novesoftware.net>

--- UniLetters/UniLetters.WebApi/Data/Migrations/20250608171533_RenameIdToAm.Designer.cs
+++ UniLetters/UniLetters.WebApi/Data/Migrations/20250608171533_RenameIdToAm.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UniLetters.WebApi.Data;
@@ -11,9 +12,11 @@ using UniLetters.WebApi.Data;
 namespace UniLetters.WebApi.Data.Migrations
 {
     [DbContext(typeof(UniLettersDbContext))]
-    partial class UniLettersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250608171533_RenameIdToAm")]
+    partial class RenameIdToAm
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UniLetters/UniLetters.WebApi/Data/Migrations/20250608171533_RenameIdToAm.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniLetters.WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameIdToAm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grade_Students_StudentId",
+                table: "Grade");
+
+            migrationBuilder.RenameColumn(
+                name: "StudentId",
+                table: "Grade",
+                newName: "Am");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Semester",
+                table: "Courses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grade_Students_Am",
+                table: "Grade",
+                column: "Am",
+                principalTable: "Students",
+                principalColumn: "Am",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grade_Students_Am",
+                table: "Grade");
+
+            migrationBuilder.DropColumn(
+                name: "Semester",
+                table: "Courses");
+
+            migrationBuilder.RenameColumn(
+                name: "Am",
+                table: "Grade",
+                newName: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grade_Students_StudentId",
+                table: "Grade",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Am",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
