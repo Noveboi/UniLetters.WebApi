fffba05134c8c5ee83a54ae48c6b9f35d00a1960
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-06-08 12:57:16 +0300

    Partially implemented CreateReferenceLetterEndpoint.cs

--- UniLetters/UniLetters.WebApi/Endpoints/CreateReferenceLetterEndpoint.cs
+++ UniLetters/UniLetters.WebApi/Endpoints/CreateReferenceLetterEndpoint.cs
@@ -1,6 +1,23 @@
+using FastEndpoints;
+using UniLetters.WebApi.Endpoints.Dto;
+using UniLetters.WebApi.Services;
+
 namespace UniLetters.WebApi.Endpoints;
 
-public class CreateReferenceLetterEndpoint
+public record CreateReferenceLetterRequest([property: RouteParam] string Am, [property: RouteParam] int LetterId);
+
+public class CreateReferenceLetterEndpoint : Endpoint<CreateReferenceLetterRequest, StudentCoursesDto>
 {
-    
+    public override void Configure()
+    {
+        Get("/students/{am}/letters/{letterId}");
+        AllowAnonymous();
+    }
+
+    public override Task HandleAsync(CreateReferenceLetterRequest req, CancellationToken ct)
+    {
+        // temp
+        var referenceLetter= new LettersService().CreateLetterAsync(req.LetterId, ct);
+        return referenceLetter;
+    }
 }
