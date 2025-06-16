a8717bcbd06d81c7cac12161b06eacb2273fbec6
George Nikolaidis <nove@novesoftware.net>
2025-06-16 19:15:16 +0300

    Add more randomness/variety to grade generation

--- UniLetters/UniLetters.WebApi/Data/Seeding/StartupService.cs
+++ UniLetters/UniLetters.WebApi/Data/Seeding/StartupService.cs
@@ -16,56 +16,65 @@ internal class StartupService(IServiceProvider sp, ILogger<StartupService> logge
 
         logger.LogInformation("Creating database and applying migrations");
         await context.Database.MigrateAsync(ct);
-
+        
         logger.LogInformation("Seeding data...");
+
+        var students = new List<Student>(FakeStudents.Get(15))
+        {
+            new("p21028", "Μαρια Δημητρουλη"),
+            new("p21151", "Κωνσταντινος Σκλαβενιτης"),
+            new("p21115", "Γιωργος Νικολαιδης")
+        };
+
+        var courses = new List<Course>
+        {
+            new("Τεχνολογία Λογισμικού", 6),
+            new("Ανάλυση Ι", 1),
+            new("Επεξεργασία Σημάτων Φωνής και Ήχου", 8),
+            new("Ανάλυση ΙΙ", 2),
+            new("Αντικειμενοστρεφής Προγραμματισμός", 2),
+            new("Εκπαιδευτικό Λογισμικό", 8),
+            new("Αλληλεπίδραση Ανθρώπου - Υπολογιστή", 5),
+            new("Πληροφοριακά Συστήματα", 5),
+            new("Βάσεις Δεδομένων", 4),
+            new("Αλγόριθμοι", 4),
+            new("Πιθανότητες και Στατιστική", 3),
+            new("Λειτουργικά Συστήματα", 3),
+            new("Βιοπληροφορική", 6),
+            new("Συστήματα Πολυμέσων", 6),
+            new("Τεχνητή Νοημοσύνη και Έμπειρα Συστήματα", 6),
+            new("Ανάλυση Εικόνας", 7),
+            new("Εικονική Πραγματικότητα", 7),
+            new("Σύγχρονα Θέματα Τεχνολογίας Λογισμικού", 7)
+        };
+        
         if (!await context.Students.AnyAsync(ct))
         {
             logger.LogInformation("Adding students");
-            context.Students.AddRange([
-                new Student("p21028", "Μαρια Δημητρουλη"),
-                new Student("p21151", "Κωνσταντινος Σκλαβενιτης"),
-                new Student("p21115", "Γιωργος Νικολαιδης"),
-                ..FakeStudents.Get(15)]);
+            context.Students.AddRange(students);
         }
 
         if (!await context.Courses.AnyAsync(ct))
         {
             logger.LogInformation("Adding courses");
-            context.Courses.AddRange(
-                new Course("Τεχνολογία Λογισμικού", 6),
-                new Course("Ανάλυση Ι", 1),
-                new Course("Επεξεργασία Σημάτων Φωνής και Ήχου", 8),
-                new Course("Ανάλυση ΙΙ", 2),
-                new Course("Αντικειμενοστρεφής Προγραμματισμός", 2),
-                new Course("Εκπαιδευτικό Λογισμικό", 8),
-                new Course("Αλληλεπίδραση Ανθρώπου - Υπολογιστή", 5),
-                new Course("Πληροφοριακά Συστήματα", 5),
-                new Course("Βάσεις Δεδομένων", 4),
-                new Course("Αλγόριθμοι", 4),
-                new Course("Πιθανότητες και Στατιστική", 3),
-                new Course("Λειτουργικά Συστήματα", 3),
-                new Course("Βιοπληροφορική", 6),
-                new Course("Συστήματα Πολυμέσων", 6),
-                new Course("Τεχνητή Νοημοσύνη και Έμπειρα Συστήματα", 6),
-                new Course("Ανάλυση Εικόνας", 7),
-                new Course("Εικονική Πραγματικότητα", 7),
-                new Course("Σύγχρονα Θέματα Τεχνολογίας Λογισμικού", 7));
+            context.Courses.AddRange(courses);
         }
 
         logger.LogInformation("Adding grades");
-        var students = await context.Students.ToListAsync(ct);
-        var courses = await context.Courses.ToListAsync(ct);
 
         foreach (var student in students)
         {
+            var proficiencyMultiplier = Random.Shared.NextDouble() * 1.5 + 0.5;
             var gradeProbability = Random.Shared.NextDouble();
 
-            foreach (var course in courses.Where(_ => Random.Shared.NextDouble() > gradeProbability))
+            foreach (var course in courses.Where(_ => Random.Shared.NextDouble() <= gradeProbability))
             {
+                var grade = Math.Min(10, proficiencyMultiplier * Random.Shared.NextDouble() * 10);
+                
                 context.Set<Grade>().Add(new Grade(
                     am: student.Am,
                     courseId: course.Id,
-                    value: double.Round(Random.Shared.NextDouble() * 10, 1)));
+                    value: double.Round(grade, 1)));
             }
         }
 
