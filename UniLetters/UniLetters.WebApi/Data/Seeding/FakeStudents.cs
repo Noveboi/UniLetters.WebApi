3eed2c429983e526d2214b26b8ffb5edabdca7a4
George Nikolaidis <nove@novesoftware.net>
2025-06-16 18:41:23 +0300

    Add Bogus for fake student generation, add more subjects

---
+++ UniLetters/UniLetters.WebApi/Data/Seeding/FakeStudents.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using UniLetters.WebApi.Domain;
+
+namespace UniLetters.WebApi.Data.Seeding;
+
+internal static class FakeStudents
+{
+    private static readonly Faker<Student> Faker = new Faker<Student>(locale: "el")
+        .RuleFor(x => x.Am, f =>
+        {
+            var year = f.Date.Past(10).Year.ToString()[2..];
+            var number = string.Join(string.Empty, f.Random.Digits(3));
+
+            return $"p{year}{number}";
+        })
+        .RuleFor(x => x.Name, f => f.Person.FullName);
+
+    public static List<Student> Get(int count) => Faker.Generate(count);
+}
--- UniLetters/UniLetters.WebApi/Data/Seeding/StartupService.cs
+++ UniLetters/UniLetters.WebApi/Data/Seeding/StartupService.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.EntityFrameworkCore;
+using UniLetters.WebApi.Data.Seeding;
 using UniLetters.WebApi.Domain;
 
 namespace UniLetters.WebApi.Data;
@@ -22,29 +23,38 @@ internal class StartupService(IServiceProvider sp, ILogger<StartupService> logge
         if (!await context.Students.AnyAsync(ct))
         {
             logger.LogInformation("Adding students");
-            context.Students.AddRange(
+            context.Students.AddRange([
                 new Student("p21028", "Μαρια Δημητρουλη"),
                 new Student("p21151", "Κωνσταντινος Σκλαβενιτης"),
-                new Student("p21115", "Γιωργος Νικολαιδης"));
+                new Student("p21115", "Γιωργος Νικολαιδης"),
+                ..FakeStudents.Get(15)]);
         }
 
         if (!await context.Courses.AnyAsync(ct))
         {
             logger.LogInformation("Adding courses");
             context.Courses.AddRange(
-                new Course("Τεχνολογια Λογισμικου", 6),
-                new Course("Αναλυση Ι", 1),
-                new Course("Επεξεργασια Σηματων Φωνης και Ηχου", 8));
+                new Course("Τεχνολογία Λογισμικού", 6),
+                new Course("Ανάλυση Ι", 1),
+                new Course("Επεξεργασία Σημάτων Φωνής και Ήχου", 8),
+                new Course("Ανάλυση ΙΙ", 2),
+                new Course("Αντικειμενοστρεφής Προγραμματισμός", 2),
+                new Course("Εκπαιδευτικό Λογισμικό", 8),
+                new Course("Αλληλεπίδραση Ανθρώπου - Υπολογιστή", 5),
+                new Course("Πληροφοριακά Συστήματα", 5),
+                new Course("Βάσεις Δεδομένων", 4),
+                new Course("Αλγόριθμοι", 4),
+                new Course("Πιθανότητες και Στατιστική", 3),
+                new Course("Λειτουργικά Συστήματα", 3),
+                new Course("Βιοπληροφορική", 6),
+                new Course("Συστήματα Πολυμέσων", 6),
+                new Course("Τεχνητή Νοημοσύνη και Έμπειρα Συστήματα", 6),
+                new Course("Ανάλυση Εικόνας", 7),
+                new Course("Εικονική Πραγματικότητα", 7),
+                new Course("Σύγχρονα Θέματα Τεχνολογίας Λογισμικού", 7));
         }
 
-        await context.SaveChangesAsync(ct);
-
-        if (await context.Set<Grade>().AnyAsync(ct))
-        {
-            return;
-        }
-
-        logger.LogInformation("Adding grades ");
+        logger.LogInformation("Adding grades");
         var students = await context.Students.ToListAsync(ct);
         var courses = await context.Courses.ToListAsync(ct);
 
